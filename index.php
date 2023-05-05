1f386631d470438c72a18eeb052906aff7f135c2
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-05 15:23:19 +0200

    add form

--- index.php
+++ index.php
@@ -8,3 +8,34 @@ Milestone 3 (BONUS)
 Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
 Milestone 4 (BONUS)
 Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme). Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<title>Strong Password Generator</title>
+</head>
+
+<body>
+
+    <form>
+        <div class="mb-3">
+            <label for="length" class="form-label">Enter the length of the password (only number)
+            </label>
+            <input type="number" class="form-control" name="length" id="length" placeholder="Enter the length of the password (only number)
+">
+            <button type="submit" class="btn btn-primary">Enter</button>
+        </div>
+    </form>
+
+</body>
+
+</html>
